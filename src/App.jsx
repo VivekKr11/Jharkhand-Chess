2e6410bd3ff98406c6b2b0fa5f4cf0d30e65d580
tanmoy-das-sbu <147819968+tanmoy-das-sbu@users.noreply.github.com>
2024-06-01 15:19:12 +0530

    Structure defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Navbar />
+      <Routes>
+      <Route path="/" element={<Home />}/>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
