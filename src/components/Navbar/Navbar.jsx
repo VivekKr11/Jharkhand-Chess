02a139afaebb9d1f9e48d3b8aeff730d41d6f5b2
Vivek Kumar <vivek2002mahto@gmail.com>
2024-06-01 15:24:57 +0530

    NAVBAR

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav className="flex w-full container m-auto">
+        <div className="p-2">
+          <img className="w-[80px]" src="./logo/logo.jpg" alt="" />
+        </div>
+        <ul className="flex justify-start gap-9 w-full  font-semibold text-[23px] text-center items-center ">
+          <li className="cursor-pointer ">About Us</li>
+          <li className="cursor-pointer">Players</li>
+          <li className="cursor-pointer">Calendar</li>
+          <li className="cursor-pointer">Information</li>
+          <li className="cursor-pointer">Affilliates</li>
+          <li className="cursor-pointer">Archives</li>
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
