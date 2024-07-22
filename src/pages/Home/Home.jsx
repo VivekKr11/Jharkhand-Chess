fe4d6b5a1c92a42ce89e6169d14b9a90d22931ba
tanmoy-das-sbu <147819968+tanmoy-das-sbu@users.noreply.github.com>
2024-07-22 12:44:23 +0530

    changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "../../css/embla.css";
 import EmblaCarousel from "../../js/EmblaCarousel.jsx";
@@ -6,43 +6,60 @@ import Info from "./Info/Info.jsx";
 import ImageGallery from "./ImageGallery.jsx";
 import News from "../../pages/Home/News/News.jsx";
 import InfiniteMovingCard from "../InfiniteMovingCard.jsx";
+import Marquee from "react-marquee-slider";
+import axios from "axios";
 
 const OPTIONS = { align: "start", loop: true };
 const SLIDES = [
-  { 
+  {
     number: "100",
-    info : "Registered Players",
+    info: "Registered Players",
     img: "./gotis/registered.svg",
-   },
-  { number: "100",
-    info : "Tournaments in 2018",
-    img: "./gotis/tournaments.svg",
-
-   },
-  { 
-    number: "100",
-    info : "International Masters",
-    img : "./gotis/international.svg"
-   },
-  { 
-    number: "100" ,
-    info : "FIDE Rated Players",
-     img : "./gotis/rated.svg"
   },
-  
+  {
+    number: "100",
+    info: "Tournaments in 2018",
+    img: "./gotis/tournaments.svg",
+  },
+  {
+    number: "100",
+    info: "International Masters",
+    img: "./gotis/international.svg",
+  },
+  {
+    number: "100",
+    info: "FIDE Rated Players",
+    img: "./gotis/rated.svg",
+  },
 ];
 
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://sbps-backend.vercel.app/home/headlines/GetAll"
+        );
+        setData(response.data.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    };
+    fetchData();
+  });
+
   return (
     <>
-      
-       <div className="bg-gradient-to-b from-[#BCADA7] via-[#FDD8B7] to-[#BA9C87] flex flex-col justify-between">
+      <div className="bg-gradient-to-b from-[#BCADA7] via-[#FDD8B7] to-[#BA9C87] flex flex-col justify-between relative">
         <div className="container mx-auto flex-grow min-h-[100vh] ">
           <div className="flex flex-col justify-start md:w-full relative">
             <div className="flex flex-col p-3 md:w-1/2 h-full">
-              <div className="mt-20 md:mt-36 mb-2 md:mb-10 font-playfair justify-center md:text-left text-center">
+              <div className="mt-20 md:mt-28 mb-2 md:mb-10 font-playfair justify-center md:text-left text-center">
                 <p className=" text-3xl md:text-[80px] font-bold text-[#2C1B16]">
-                  All  Jharkhand
+                  All Jharkhand
                 </p>
                 <p className="text-[88px] md:text-[150px] font-extrabold text-[#2C1B16]">
                   CHESS{" "}
@@ -80,15 +97,40 @@ const Home = () => {
             </div>
           </div>
           <div className="container flex justify-center  md:justify-end">
-            <div className= "">
+            <div className="mb-20 md:mb-10">
               <EmblaCarousel slides={SLIDES} options={OPTIONS} />
             </div>
           </div>
         </div>
+        <div className="max-w-full pt-4 absolute bottom-0 mx-auto flex items-center">
+  <div className="bg-[#2C1B16] text-white text-xs md:text-lg md:py-2 py-1 px-2 whitespace-nowrap">
+    Latest News
+  </div>
+  <div id="Headline-div" className="overflow-x-hidden custom-gradient py-0 md:py-2 flex-1">
+    <Marquee className="py-2" pauseOnHover="pause" speed={100}>
+      {Array.isArray(data) &&
+        data.map((item, index) => (
+          <span
+            key={index}
+            className="md:text-lg text-xs mx-6 text-yellow-900 font-bold inline-block ml-5"
+          >
+            <img
+              src="/star.svg"
+              alt="Star"
+              className="inline-block md:w-6 md:h-6 w-4 h-4 mr-2"
+            />
+            {item.description}
+          </span>
+        ))}
+    </Marquee>
+  </div>
+</div>
+
       </div>
       <div className="mt-[20px] md:mt-[50px] container mx-auto">
         <Info />
       </div>
+
       <News />
       <div>
         <ImageGallery />
--- src/pages/InfiniteMovingCard.jsx
+++ src/pages/InfiniteMovingCard.jsx
@@ -43,8 +43,8 @@ const InfiniteMovingCard = () => {
     <div className="h-[20rem] rounded-md flex flex-col antialiased bg-white dark:bg-white dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
       <InfiniteMovingCards
         items={testimonials}
-        direction="right"
-        speed="slow"
+        direction="left"
+        speed="fast"
       />
       <img src="" alt="" />
     </div>
