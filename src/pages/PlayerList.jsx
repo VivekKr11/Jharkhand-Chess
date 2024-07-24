7e08964994d949a451ef6a4cde8f94c7e720754e
tanmoy-das-sbu <147819968+tanmoy-das-sbu@users.noreply.github.com>
2024-07-24 11:31:55 +0530

    chnages in sorting, player list

--- src/pages/PlayerList.jsx
+++ src/pages/PlayerList.jsx
@@ -5,7 +5,7 @@ const PlayerList = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [sortConfig, setSortConfig] = useState({
-    key: null,
+    key: "Name",
     direction: "ascending",
   });
 
@@ -15,7 +15,12 @@ const PlayerList = () => {
         const response = await axios.get(
           "https://jharkhand-chess-backend.vercel.app/Registration/GetAll"
         );
-        setData(response.data);
+        const sortedData = response.data.sort((a, b) => {
+          let aValue = a.Name.toLowerCase();
+          let bValue = b.Name.toLowerCase();
+          return aValue < bValue ? -1 : aValue > bValue ? 1 : 0;
+        });
+        setData(sortedData);
         setLoading(false); // Set loading to false after data is fetched
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -69,14 +74,26 @@ const PlayerList = () => {
       </div>
       <div className="container mx-auto overflow-x-auto">
         {loading ? (
-          <div className="flex justify-center items-center h-64">
-          <div role="status">
-    <svg aria-hidden="true" class="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-[#2C1B16]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-    </svg>
-    <span class="sr-only">Loading...</span>
-</div>
+          <div className="flex justify-center items-center h-[50vh]">
+            <div role="status">
+              <svg
+                aria-hidden="true"
+                className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-[#2C1B16]"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
           </div>
         ) : (
           <table className="min-w-full bg-[#2C1B16] border-x-2 border-[#2C1B16] rounded-lg shadow-md">
@@ -86,7 +103,7 @@ const PlayerList = () => {
                   className="px-6 py-3 flex flex-row  justify-between text-left text-lg font-medium text-white uppercase tracking-wider cursor-pointer"
                   onClick={() => requestSort("Name")}
                 >
-                  Name <svg width="40px" height="40px" viewBox="-2.4 -2.4 28.80 28.80" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16.0686 15H7.9313C7.32548 15 7.02257 15 6.88231 15.1198C6.76061 15.2238 6.69602 15.3797 6.70858 15.5393C6.72305 15.7232 6.93724 15.9374 7.36561 16.3657L11.4342 20.4344C11.6323 20.6324 11.7313 20.7314 11.8454 20.7685C11.9458 20.8011 12.054 20.8011 12.1544 20.7685C12.2686 20.7314 12.3676 20.6324 12.5656 20.4344L16.6342 16.3657C17.0626 15.9374 17.2768 15.7232 17.2913 15.5393C17.3038 15.3797 17.2392 15.2238 17.1175 15.1198C16.9773 15 16.6744 15 16.0686 15Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M7.9313 9.00005H16.0686C16.6744 9.00005 16.9773 9.00005 17.1175 8.88025C17.2393 8.7763 17.3038 8.62038 17.2913 8.46082C17.2768 8.27693 17.0626 8.06274 16.6342 7.63436L12.5656 3.56573C12.3676 3.36772 12.2686 3.26872 12.1544 3.23163C12.054 3.199 11.9458 3.199 11.8454 3.23163C11.7313 3.26872 11.6323 3.36772 11.4342 3.56573L7.36561 7.63436C6.93724 8.06273 6.72305 8.27693 6.70858 8.46082C6.69602 8.62038 6.76061 8.7763 6.88231 8.88025C7.02257 9.00005 7.32548 9.00005 7.9313 9.00005Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                  Name{" "}<svg width="40px" height="40px" viewBox="-2.4 -2.4 28.80 28.80" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16.0686 15H7.9313C7.32548 15 7.02257 15 6.88231 15.1198C6.76061 15.2238 6.69602 15.3797 6.70858 15.5393C6.72305 15.7232 6.93724 15.9374 7.36561 16.3657L11.4342 20.4344C11.6323 20.6324 11.7313 20.7314 11.8454 20.7685C11.9458 20.8011 12.054 20.8011 12.1544 20.7685C12.2686 20.7314 12.3676 20.6324 12.5656 20.4344L16.6342 16.3657C17.0626 15.9374 17.2768 15.7232 17.2913 15.5393C17.3038 15.3797 17.2392 15.2238 17.1175 15.1198C16.9773 15 16.6744 15 16.0686 15Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M7.9313 9.00005H16.0686C16.6744 9.00005 16.9773 9.00005 17.1175 8.88025C17.2393 8.7763 17.3038 8.62038 17.2913 8.46082C17.2768 8.27693 17.0626 8.06274 16.6342 7.63436L12.5656 3.56573C12.3676 3.36772 12.2686 3.26872 12.1544 3.23163C12.054 3.199 11.9458 3.199 11.8454 3.23163C11.7313 3.26872 11.6323 3.36772 11.4342 3.56573L7.36561 7.63436C6.93724 8.06273 6.72305 8.27693 6.70858 8.46082C6.69602 8.62038 6.76061 8.7763 6.88231 8.88025C7.02257 9.00005 7.32548 9.00005 7.9313 9.00005Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                 </th>
                 <th
                   className="px-6 py-3  text-left text-lg font-medium text-white uppercase tracking-wider cursor-pointer"
@@ -111,16 +128,16 @@ const PlayerList = () => {
             <tbody className="bg-[#F8F4E1] divide-y divide-gray-400">
               {sortedData.map((item, index) => (
                 <tr key={index}>
-                  <td className="px-6 py-4 border-x-2 border-[#2C1B16] whitespace-nowrap text-sm font-semibold text-neutral-700">
+                  <td className="px-6 py-4 border-x-2 border-y-2 border-[#2C1B16] whitespace-nowrap text-sm font-semibold text-neutral-700">
                     {item.Name}
                   </td>
-                  <td className="px-6 py-4 border-x-2 border-[#2C1B16] whitespace-nowrap text-sm font-semibold text-neutral-700">
+                  <td className="px-6 py-4 border-x-2 border-y-2 border-[#2C1B16] whitespace-nowrap text-sm font-semibold text-neutral-700">
                     {item.FideId}
                   </td>
-                  <td className="px-6 py-4 border-x-2 border-[#2C1B16] whitespace-nowrap text-sm font-semibold text-neutral-700">
+                  <td className="px-6 py-4 border-x-2 border-y-2  border-[#2C1B16] whitespace-nowrap text-sm font-semibold text-neutral-700">
                     {item.Dist}
                   </td>
-                  <td className="px-6 py-4 border-x-2 border-[#2C1B16] whitespace-nowrap text-sm font-semibold text-neutral-700">
+                  <td className="px-6 py-4 border-x-2 border-y-2 border-[#2C1B16] whitespace-nowrap text-sm font-semibold text-neutral-700">
                     {item.AicfID}
                   </td>
                 </tr>
