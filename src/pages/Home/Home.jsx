4fee2fb68e030d64fc6a75f5f5166f7caef28785
tanmoy-das-sbu <147819968+tanmoy-das-sbu@users.noreply.github.com>
2024-07-24 10:27:48 +0530

    changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom/client";
-import "../../css/embla.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import Marquee from "react-marquee-slider";
 import EmblaCarousel from "../../js/EmblaCarousel.jsx";
 import Info from "./Info/Info.jsx";
 import ImageGallery from "./ImageGallery.jsx";
 import News from "../../pages/Home/News/News.jsx";
 import InfiniteMovingCard from "../InfiniteMovingCard.jsx";
-import Marquee from "react-marquee-slider";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import "../../css/embla.css";
 
 const OPTIONS = { align: "start", loop: true };
 const SLIDES = [
@@ -33,25 +31,14 @@ const SLIDES = [
     img: "./gotis/rated.svg",
   },
 ];
+const data = [
+  "Aditya Birla Memorial: 23rd Jharkhand State FIDE Rating Chess Championship 2024 to be Held from 20th to 23rd July",
+  "23rd Jharkhand State FIDE Rating Chess Championship: Eligibility, Rules, and Prize Details Announced",
+  "Register Now for the Jharkhand State FIDE Chess Championship 2024: Key Dates and Entry Requirements",
+  "Jharkhand Chess Championship 2024: Prizes, Programme, and Accommodation Details Released",
+];
 
 const Home = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://sbps-backend.vercel.app/home/headlines/GetAll"
-        );
-        setData(response.data.data);
-        console.log(response.data);
-      } catch (error) {
-        console.error("Error fetching data", error);
-      }
-    };
-    fetchData();
-  }, []); // Added an empty dependency array to ensure useEffect runs only once
-
   return (
     <>
       <div className="bg-gradient-to-b from-[#BCADA7] via-[#FDD8B7] to-[#BA9C87] flex flex-col justify-between relative min-h-screen">
@@ -78,10 +65,10 @@ const Home = () => {
                   Association
                 </p>
               </div>
-              <div className="flex justify-center md:justify-start py-2">
+              <div className="flex justify-center md:justify-start gap-10 py-2">
                 <Link to="/registrationform">
                   <button className="bg-[#2C1B16] text-white md:p-3 p-2 px-2 md:w-60 rounded-md md:rounded-2xl gap-1 md:font-bold flex justify-start transform transition-transform duration-300 hover:scale-110 text-sm md:text-lg  md:mt-6">
-                    Chess Festival-2024{" "}
+                    Chess Festival-2024
                     <img
                       className="h-[20px] md:h-[30px]"
                       src="./props/chesspion.svg"
@@ -89,6 +76,16 @@ const Home = () => {
                     />
                   </button>
                 </Link>
+                <a href="/pdf/23rd-Jharkhand-State-FIDE-Rating-Chess-Championship-2024.pdf" target="_blank">
+                  <button className="bg-[#2C1B16] text-white md:p-3 p-2 px-2 md:w-fit rounded-md md:rounded-2xl gap-1 md:font-bold flex justify-start transform transition-transform duration-300 hover:scale-110 text-sm md:text-lg  md:mt-6">
+                    Brochure
+                    <img
+                      className="h-[20px] md:h-[30px]"
+                      src="./props/chesspion.svg"
+                      alt=""
+                    />
+                  </button>
+                </a>
               </div>
             </div>
             <div className="md:absolute right-9 -top-16 flex items-center">
@@ -110,15 +107,24 @@ const Home = () => {
             <div className="bg-[#2C1B16] text-white text-xs md:text-lg md:py-2 py-1 px-2 whitespace-nowrap">
               Latest News
             </div>
-            <div id="Headline-div" className="overflow-x-hidden custom-gradient py-0 md:py-2 flex-1">
+            <div
+              id="Headline-div"
+              className="overflow-x-hidden custom-gradient py-0 md:py-2 flex-1"
+            >
               <Marquee className="py-2" pauseOnHover={true} speed={100}>
-                {Array.isArray(data) &&
-                  data.map((item, index) => (
-                    <span key={index} className="md:text-lg text-xs mx-6 text-yellow-900 font-bold inline-block ml-5">
-                      <img src="/star.svg" alt="Star" className="inline-block md:w-6 md:h-6 w-4 h-4 mr-2"/>
-                      {item.description}
-                    </span>
-                  ))}
+                {data.map((item, index) => (
+                  <span
+                    key={index}
+                    className="md:text-lg text-xs mx-6 text-yellow-900 font-bold inline-block ml-5"
+                  >
+                    <img
+                      src="/star.svg"
+                      alt="Star"
+                      className="inline-block md:w-6 md:h-6 w-4 h-4 mr-2"
+                    />
+                    {item}
+                  </span>
+                ))}
               </Marquee>
             </div>
           </div>
